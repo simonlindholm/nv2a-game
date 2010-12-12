249190d2946134e16766a0cfb1f8782358900117
Nicole <immaterialism@hotmail.com>
2010-12-12 19:57:59 +0100

    Added another macro to make shared pointers work for me.

--- src/shared_ptr.h
+++ src/shared_ptr.h
@@ -3,12 +3,16 @@
 // Include file for shared pointers, which are not yet standardized.
 // (You may need to change this for other compilers.)
 
-#ifdef STD_SHARED_PTR
+#ifdef SHARED_PTR_MEMORY
 #include <memory>
+#else
+#include <tr1/memory>
+#endif
+
+#ifdef SHARED_PTR_STD
 using std::shared_ptr;
 using std::weak_ptr;
 #else
-#include <tr1/memory>
 using std::tr1::shared_ptr;
 using std::tr1::weak_ptr;
 #endif
